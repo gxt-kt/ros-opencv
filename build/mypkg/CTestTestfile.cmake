4de5b73fcc7c043966841e9a70733fe655f821fa
gxt_kt <gxt_kt@163.com>
2022-06-05 02:45:03 +0800

    rm build

--- build/mypkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kt/Desktop/ros_test/src/mypkg
-# Build directory: /home/kt/Desktop/ros_test/build/mypkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
